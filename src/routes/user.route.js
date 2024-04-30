d5577a4f45ac890dc62a5fa9c0d0964fcaf9953f
Hrushikesh <162966657+HrushikeshB-1121@users.noreply.github.com>
2024-04-30 11:14:07 +0530

    "[Hrushikesh] Added:SignUp Or Login"

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,8 +5,7 @@ import { newUserValidator } from '../validators/user.validator';
 const router = express.Router();
 
 //route to Register a new user
-router.post('', newUserValidator, userController.newUser);
-
+router.post('/signup', newUserValidator, userController.newUser);
 
 
 router.post('/login', userController.userLogin);

5e49b9eaeffe574ee717f4c8da23e2d453d68289
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-05-02 16:36:29 +0530

    [Hrushikesh] Added:interacts with database

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,30 +3,42 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
-//create new user
-export const newUser = async (body) => {
+const key = process.env.SECRET_KEY;
 
-  
-  const userExists= await User.findOne({email:body.email})
-  
-  if(userExists){
-    throw new Error('User with this email already exists')
-  }else{
-  body.password=await bcrypt.hash(body.password,10)
-  const data = await User.create(body);
-  return data;
+export const userSignIn = async (req) => {
+  const body=req.body
+  const userExists = await User.findOne({ email: body.email });
+  if (userExists) {
+    throw new Error('User already exists');
+  } else {
+    body.password = await bcrypt.hash(body.password, 10);
+    const data = await User.create(body);
+    return data;
   }
 };
 
-export const userLogin=async ({ email, password }) => {
+export const userLogin = async (req) => {
+  const { email, password }=req.body
 
   const user = await User.findOne({ email });
 
   if (!user || !(await bcrypt.compare(password, user.password))) {
     throw new Error('Invalid email or password');
   }
+  
+  const token = jwt.sign({ userId: user._id }, key, { expiresIn: '1h' });
+  return { user, token };
+};
 
-  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
-
-  return {user,token}
+export const verifyUser = async (res) => {
+  const {token,userId} = res.locals;
+  if (!token) {
+    throw new Error('Token Not provided');
+  }
+  try {
+    const user = await User.findById(userId);
+    return { user, token };
+  } catch (error) {
+    throw new Error('Invalid token');
+  }
 };

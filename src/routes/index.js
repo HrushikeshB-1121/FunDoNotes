18278e516856d9e8a60d65cd30c6c0187f486570
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-05-02 16:31:43 +0530

    [Hrushikesh] Added:index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,19 @@ import express from 'express';
 const router = express.Router();
 
 import userRoute from './user.route';
+import notesRoute from './notes.route';
 /**
  * Function contains Application routes
  *
  * @returns router
  */
 const routes = () => {
-  router.get('/', (req, res) => {
-    res.json('Welcome');
-  });
+
+  // route for userCreation, login and token
   router.use('/users', userRoute);
+  
+  // route for notes creation, archive and trash
+  router.use('/notes', notesRoute);
 
   return router;
 };

a090a6d7685f9411a05629fa3727479a885fe03a
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-05-02 16:25:22 +0530

    [Hrushikesh] Added:notes controller

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -51,3 +51,52 @@ export const isTrashedNote = async (req, res) => {
     });
   }
 };
+
+export const updateDesc = async (req, res) => {
+  try {
+    const data = await NoteService.updateDesc(req,res);
+    res.status(HttpStatus.CREATED).json({
+      code: HttpStatus.CREATED,
+      data: data,
+      message: 'Note updated successfully'
+    });
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: error.message
+    });
+  }
+};
+
+export const deleteNote = async (req, res) => {
+  try {
+    const data = await NoteService.deleteNote(req,res);
+    res.status(HttpStatus.CREATED).json({
+      code: HttpStatus.CREATED,
+      data: data,
+      message: 'Note Deleted successfully'
+    });
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: error.message
+    });
+  }
+};
+
+
+export const getAllNote = async (req, res) => {
+    try {
+      const data = await NoteService.getAllNote(req,res);
+      res.status(HttpStatus.CREATED).json({
+        code: HttpStatus.CREATED,
+        data: data,
+        message: 'Displayed all notes successfully'
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        code: HttpStatus.BAD_REQUEST,
+        message: error.message
+      });
+    }
+  };

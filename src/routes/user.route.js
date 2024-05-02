1fcbbfca615a41227b309aee7609aff849d3f0d2
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-05-02 16:35:00 +0530

    [Hrushikesh] Added:routes on users

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
-import { newUserValidator } from '../validators/user.validator';
+import { signinDetailsValidator,loginDetailsValidator} from '../validators/user.validator';
+import { userAuth ,isAuthBySession } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
-//route to Register a new user
-router.post('/signup', newUserValidator, userController.newUser);
+// route to Register a new user
+router.post('', signinDetailsValidator, userController.userSignIn);
 
+// route to login user
+router.post('/login',loginDetailsValidator, userController.userLogin);
 
-
-router.post('/login', userController.userLogin);
-
+// route to verify user (using JWT)
+router.get('/verify', userAuth , userController.verifyUser);
 
 export default router;

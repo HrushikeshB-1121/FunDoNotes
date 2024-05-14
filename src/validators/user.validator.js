d20075dc929d493e2cd1a6dea56c3332914bd9be
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-05-14 14:14:53 +0530

    [Hrushikesh] Updated:email is now searched case-insensitively

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,8 +1,8 @@
 import Joi from '@hapi/joi';
 import HttpStatus from 'http-status-codes';
+import logger from '../config/logger';
 
-
-export const signinDetailsValidator = (req, res, next) => {
+export const signIn = (req, res, next) => {
   const schema = Joi.object({
     firstName: Joi.string().min(4).required(),
     lastName: Joi.string().min(4).required(),
@@ -13,6 +13,7 @@ export const signinDetailsValidator = (req, res, next) => {
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
+    logger.error(`error while creating user`)
     res.status(HttpStatus.BAD_REQUEST).json({
       code: HttpStatus.BAD_REQUEST,
       message: error.message
@@ -23,7 +24,7 @@ export const signinDetailsValidator = (req, res, next) => {
   }
 };
 
-export const loginDetailsValidator = (req, res, next) => {
+export const logIn = (req, res, next) => {
   const schema = Joi.object({
     email: Joi.string().email().message('Not a valid Email').required(),
     password: Joi.string().min(8).pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?!.*\s).{8,}$/)
@@ -43,7 +44,7 @@ export const loginDetailsValidator = (req, res, next) => {
   }
 };
 
-export const emailValid = (req, res, next) => {
+export const email = (req, res, next) => {
   const schema = Joi.object({
     email: Joi.string().email().message('Not a valid Email').required()
   });
@@ -59,7 +60,7 @@ export const emailValid = (req, res, next) => {
   }
 };
 
-export const passwordValid = (req, res, next) => {
+export const password = (req, res, next) => {
   const schema = Joi.object({
     password: Joi.string().min(8).pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?!.*\s).{8,}$/)
     .message('Password must be 8 characters or more with at least one digit, one lowercase letter, one uppercase letter, and one special character')

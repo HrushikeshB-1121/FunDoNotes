67df550f16726d65f898cb4e091b32cf5f754cd2
HrushikeshB-1121 <hrushikeshb2545@gmail.com>
2024-05-21 13:51:25 +0530

    [Hrushikesh] Refactor:Removed extra variables

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -10,8 +10,7 @@ const key = process.env.SECRET_KEY;
 const resetkey = process.env.RESET_KEY;
 
 
-export const userSignIn = async (req) => {
-  const body=req.body
+export const userSignIn = async (body) => {
   const email = body.email.toLowerCase();
   const userExists = await User.findOne({ email: email });
   if (userExists) {
@@ -23,19 +22,18 @@ export const userSignIn = async (req) => {
   }
 };
 
-export const userLogin = async (req) => {
-  const { email, password }=req.body
-  const user = await User.findOne({ email });
-  if (!user || !(await bcrypt.compare(password, user.password))) {
+export const userLogin = async (body) => {
+  body.email = body.email.toLowerCase();
+  const user = await User.findOne({email: body.email});
+  if (!user || !(await bcrypt.compare(body.password, user.password))) {
     throw new Error('Invalid email or password');
   }
-  
   const token = jwt.sign({ userId: user._id }, key, { expiresIn: '1h' });
   return { user, token };
 };
 
 
-export const forgetPassword= async ({email}) => {
+export const forgetPassword= async (email) => {
   
   const user = await User.findOne({email});
   if(!user)
@@ -45,8 +43,7 @@ export const forgetPassword= async ({email}) => {
   return { user ,token ,result };
 };
 
-export const resetPassword= async (newPassword,res) => {
-  const userId = res.locals.userId
+export const resetPassword= async (newPassword,userId) => {
   const user = await User.findById(userId);
   if (!user) {
     logger.info(`User not found`)
